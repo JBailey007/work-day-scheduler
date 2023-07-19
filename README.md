5cf35a09310b36507de9933912c65177a8965523
Jalen Bailey <jwbailey13@gmail.com>
2023-07-19 16:56:53 -0600

    Added local storage code so that the items in the box will be saved and shown when the page is reloaded

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # work-day-scheduler
+
+## Description
+This webpage was created/edited using HTML and CSS and JavaScript Code. 
+This webpage contains a Daily Planner where you can write down items that are needed to do. 
+When a time has passed it will be highlighted in gray. 
+When it is the current time the text area box will be highlighted red. 
+When the item is in the future it will be highlighted green. 
+
+## Installation
+
+You can access this page using the following link. ()
+
+## Usage
+At the top of the page you can see the current date and time that is updating each second. 
+Click into a time box to fill out what you would like to plan for that hour segment.
+When you are finished filling out the text area you can save your work by clicking on the blue save button on the left of the page that corresponds to the time you filled out. 
+This data will be saved to your local storage so that you can come back and use the planner throughout the day. 
+
+# Credits
+N/A
+
+# Screenshot
+![Screenshot of Website for Developer Portfolio](<Screenshot 2023-06-26 151805.png>)
+
+## License
+Please refer to the LICENSE in the repo. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//variables for the dayjs function.
 var rootEl = $('#root');
 var hoursEl = rootEl.children('div');
 var today = dayjs();
 var currentHour = dayjs().format('HH');
 
+//function to run the time on the load of the page once the document is ready.
 $(document).ready(function () {
-
     $('#display-time').text(today.format('dddd, MMMM D, YYYY h:mm:ss A'));
 console.log(currentHour);
-
 function Time(){
     var today = dayjs();
 $('#display-time').text(today.format('dddd, MMMM D, YYYY h:mm:ss A'));
@@ -18,6 +15,7 @@ $('#display-time').text(today.format('dddd, MMMM D, YYYY h:mm:ss A'));
 setInterval(Time, 1000);
 });
 
+// function to run through each div and assign a class to each of them based on the time. 
 $(".time-div").each(function () {
     var timeDiv = $(this).attr("id").split("-")[1];
     
@@ -33,18 +31,22 @@ $(".time-div").each(function () {
     }
   });
 
-    
+// code to take the values that are input to the .timeblock class and add them to the local storage. 
+$(".saveBtn").click(function (event) {
+  event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
 
-      // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    // look up how to get the input - input from the particular div. That is how to get the input and then save to local storage. 
+});
 
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+//code to display the saved items back on the page after reload. 
+$("#hour-09 .time-block").val(localStorage.getItem("09"));
+$("#hour-10 .time-block").val(localStorage.getItem("10"));
+$("#hour-11 .time-block").val(localStorage.getItem("11"));
+$("#hour-12 .time-block").val(localStorage.getItem("12"));
+$("#hour-13 .time-block").val(localStorage.getItem("13"));
+$("#hour-14 .time-block").val(localStorage.getItem("14"));
+$("#hour-15 .time-block").val(localStorage.getItem("15"));
+$("#hour-16 .time-block").val(localStorage.getItem("16"));
+$("#hour-17 .time-block").val(localStorage.getItem("17"));
